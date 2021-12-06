cd84a58f89580a34fa7194eb2177af49fbfa584d
DatPT <phamdat.baron@gmail.com>
2021-12-06 17:14:40 +0700

    x

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+    def vn input
+        case input
+        when true
+            return "Đúng"
+        when false
+            return "Sai"
+        else
+           return input 
+        end
+    end
 end
